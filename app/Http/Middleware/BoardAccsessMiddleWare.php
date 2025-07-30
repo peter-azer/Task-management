c43e2c5a4955dce1d17ad3310e01d8e013e8cd4b
moofx19 <mahmoudofx@gmail.com>
2025-07-30 17:42:38 +0300

    fix super admin access to boards

--- app/Http/Middleware/BoardAccsessMiddleWare.php
+++ app/Http/Middleware/BoardAccsessMiddleWare.php
@@ -15,10 +15,10 @@ class BoardAccsessMiddleWare
     public function __construct(protected TeamLogic $teamLogic) {}
     public function handle(Request $request, Closure $next): Response
     {
-        $user_id = Auth::user()->id;
+        $user = Auth::user();
         $team_id = intval($request->route('team_id'));
         $board_id = intval($request->route('board_id'));
-        if(!$this->teamLogic->userHasAccsess($user_id, $team_id)){
+        if(!$user->hasRole("super-admin") && !$this->teamLogic->userHasAccsess($user->id, $team_id)){
             return redirect()->route("home")->with("notif", ["Problem\nThe team is not found or you have been kicked out, please contact the owner."]);
         }
 
