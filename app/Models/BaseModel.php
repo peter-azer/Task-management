883ebfe539d9be08a39ebb40631c84a055f8f817
AbdullaSami <alattariabdulla@gmail.com>
2026-03-01 13:42:18 +0200

    add activity logging functionality with Spatie package and create migration for activity log table

---
+++ app/Models/BaseModel.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\Traits\LogsActivity;
+use Spatie\Activitylog\LogOptions;
+
+class BaseModel extends Model
+{
+    use LogsActivity;
+
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()
+            ->logAll()
+            ->logOnlyDirty()
+            ->dontSubmitEmptyLogs()
+            ->useLogName($this->getTable());
+    }
+
+    public function getDescriptionForEvent(string $eventName): string
+    {
+        return "{$eventName} on " . class_basename(class: $this);
+    }
+}
